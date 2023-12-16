01b720fa135016794c629bc9e1a0f4e0f5326307
HuangJie <wsgs0705@163.com>
2023-12-16 15:54:40 +0800

    commit Calculator

--- src/main/java/com/kent/Calculator.java
+++ src/main/java/com/kent/Calculator.java
@@ -1,13 +1,151 @@
 package com.kent;
 
 /**
- * Hello world!
+ * 计算器类（Calculator），可以实现两个数的加、减、乘、除运算，并可以进行undo和redo操作
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Stack;
+
+class Calculator {
+    private double currentValue = 0.0;
+    private Stack<Operation> history = new Stack<>();
+    private Stack<Operation> redoStack = new Stack<>();
+
+    private abstract class Operation {
+        protected double value;
+
+        public Operation(double value) {
+            this.value = value;
+        }
+
+        public abstract void apply();
+
+        public abstract void undo();
+
+        public abstract void redo();
+    }
+
+    private class AddOperation extends Operation {
+        public AddOperation(double value) {
+            super(value);
+        }
+
+        public void apply() {
+            currentValue += value;
+        }
+
+        public void undo() {
+            currentValue -= value;
+        }
+
+        public void redo() {
+            apply();
+        }
+    }
+
+    private class SubtractOperation extends Operation {
+        public SubtractOperation(double value) {
+            super(value);
+        }
+
+        public void apply() {
+            currentValue -= value;
+        }
+
+        public void undo() {
+            currentValue += value;
+        }
+
+        public void redo() {
+            apply();
+        }
+    }
+
+    private class MultiplyOperation extends Operation {
+        public MultiplyOperation(double value) {
+            super(value);
+        }
+
+        public void apply() {
+            currentValue *= value;
+        }
+
+        public void undo() {
+            currentValue /= value;
+        }
+
+        public void redo() {
+            apply();
+        }
+    }
+
+    private class DivideOperation extends Operation {
+        public DivideOperation(double value) {
+            super(value);
+        }
+
+        public void apply() {
+            if (value != 0) {
+                currentValue /= value;
+            } else {
+                throw new IllegalArgumentException("被除数不能为0!");
+            }
+        }
+
+        public void undo() {
+            currentValue *= value;
+        }
+
+        public void redo() {
+            apply();
+        }
+    }
+
+    public void add(double value) {
+        AddOperation op = new AddOperation(value);
+        op.apply();
+        history.push(op);
+        redoStack.clear();
+    }
+
+    public void subtract(double value) {
+        SubtractOperation op = new SubtractOperation(value);
+        op.apply();
+        history.push(op);
+        redoStack.clear();
+    }
+
+    public void multiply(double value) {
+        MultiplyOperation op = new MultiplyOperation(value);
+        op.apply();
+        history.push(op);
+        redoStack.clear();
+    }
+
+    public void divide(double value) {
+        DivideOperation op = new DivideOperation(value);
+        op.apply();
+        history.push(op);
+        redoStack.clear();
+    }
+
+    public void undo() {
+        if (!history.isEmpty()) {
+            Operation op = history.pop();
+            op.undo();
+            redoStack.push(op);
+        }
+    }
+
+    public void redo() {
+        if (!redoStack.isEmpty()) {
+            Operation op = redoStack.pop();
+            op.redo();
+            history.push(op);
+        }
+    }
+
+    public double getCurrentValue() {
+        return currentValue;
     }
 }
--- src/test/java/com/kent/CalculatorTest.java
+++ src/test/java/com/kent/CalculatorTest.java
@@ -1,20 +1,34 @@
 package com.kent;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for Calculator.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class CalculatorTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testCaculator() {
+        Calculator calc = new Calculator();
+
+        calc.add(10);
+        Assert.assertEquals(10.0, calc.getCurrentValue(), 1.0);
+
+        calc.subtract(2);
+        Assert.assertEquals(8.0, calc.getCurrentValue(), 0.0);
+
+        calc.multiply(2);
+        Assert.assertEquals(16.0, calc.getCurrentValue(), 0.0);
+
+        calc.divide(4);
+        Assert.assertEquals(4.0, calc.getCurrentValue(), 0.0);
+
+        calc.undo();
+        Assert.assertEquals(16.0, calc.getCurrentValue(), 0.0);
+
+        calc.redo();
+        Assert.assertEquals(4.0, calc.getCurrentValue(), 0.0);
+
     }
 }
